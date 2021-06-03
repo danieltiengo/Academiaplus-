ca53698333121c9d5ddd07516c132a7175c54edc
danieltiengo <danieltiengo1@gmail.com>
2021-06-02 21:25:58 -0300

    02/06/2021

--- academiaplus/src/app/pages/endereco-add/endereco-add.page.ts
+++ academiaplus/src/app/pages/endereco-add/endereco-add.page.ts
@@ -25,6 +25,7 @@ export class EnderecoAddPage implements OnInit {
 
   ngOnInit() {
     this.userkey = this.activadeRouter.snapshot.paramMap.get('key');
+    this.endereco.userkey = this.userkey;
     //this.getendereco(this.key)
   }
 
@@ -68,22 +69,7 @@ export class EnderecoAddPage implements OnInit {
   salvar() {
     try {
       this.msg.presentLoading();
-      if (this.userkey) {
-        this.enderecoService.update(this.endereco, this.userkey).then(
-          res => {
-            console.log('Dados Salvos firebase...', res);
-            this.msg.dismissLoading();
-            this.msg.presentAlert('Alerta', 'Usuário atualizado.');
-            this.endereco = new Endereco();
-            this.router.navigate(['']);
-          },
-          error => {
-            console.error("Erro ao salvar.", error);
-            this.msg.dismissLoading();
-            this.msg.presentAlert("Error", "Não foi possivel atualizar.");
-          }
-        )
-      } else {
+      
         this.enderecoService.add(this.endereco).then(
           res => {
             console.log('Dados Salvos firebase...', res);
@@ -98,7 +84,7 @@ export class EnderecoAddPage implements OnInit {
             this.msg.presentAlert("Error", "Não foi possivel salvar.");
           }
         )
-      }
+      
     } catch (error) {
       console.error("Erro ao salvar.", error);
       this.msg.dismissLoading();
