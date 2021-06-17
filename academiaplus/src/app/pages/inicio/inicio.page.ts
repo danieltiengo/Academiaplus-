842f4f61ab884f41aa96888a978f66ccbccc8e34
lLukkil <lucasantana.1501@gmail.com>
2021-06-17 04:21:33 -0300

    17/06/2021

--- academiaplus/src/app/pages/inicio/inicio.page.ts
+++ academiaplus/src/app/pages/inicio/inicio.page.ts
@@ -8,8 +8,18 @@ import { Component, OnInit } from '@angular/core';
 export class InicioPage implements OnInit {
 
   constructor() { }
+   doRefresh(event) {
+    console.log('Begin async operation');
 
+    setTimeout(() => {
+      console.log('Async operation has ended');
+      event.target.complete();
+    }, 2000);
+  }
+  
   ngOnInit() {
   }
 
 }
+
+
